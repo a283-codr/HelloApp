ed215c3593bb1d25b71e688181e051cf1b8cee71
a283-codr <a91106283@gmail.com>
2026-03-20 00:50:28 +0530

    Update uc2_HelloApp.java

--- Hello App/src/main/java/org/example/uc2_HelloApp.java
+++ Hello App/src/main/java/org/example/uc2_HelloApp.java
@@ -1,4 +1,12 @@
-package org.example;
+// UC2: Display Hello with Command Line Argument
 
 public class uc2_HelloApp {
-}
+
+    public static void main(String[] args) {
+
+        // Check if user provided a name
+   
+            String name = args[0];
+            System.out.println("Hello " + name);
+        } 
+    }
