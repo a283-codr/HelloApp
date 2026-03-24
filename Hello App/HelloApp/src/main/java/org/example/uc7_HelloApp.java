2871469b15f0180baaa0fd66a221afd6ca5833e3
Abhinav Prasad Singh <a91106283@gmail.com>
2026-03-25 00:08:49 +0530

    added uc7 code

--- Hello App/HelloApp/src/main/java/org/example/uc7_HelloApp.java
+++ Hello App/HelloApp/src/main/java/org/example/uc7_HelloApp.java
@@ -1,4 +1,4 @@
-public class uc7_HelloApp {
+public class uc7_Helloworld {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello, World!");
